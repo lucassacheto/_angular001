703017222285f70e78d4613c1e44cd83c57e0d92
Lucas <lucas83@gmail.com>
2018-11-09 03:26:57 -0500

    almost there v0.1
    Almost there ...

--- src/app/name-color/name-color.component.ts
+++ src/app/name-color/name-color.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { getClosureSafeProperty } from '@angular/core/src/util/property';
+import { $ } from 'protractor';
+
 
 @Component({
   selector: 'formInputName',
@@ -9,10 +12,13 @@ import { Component, OnInit } from '@angular/core';
     <input type="text" id="inputName" class="form-control" autofocus />
     <br />
     <button (click)="getName()" type="submit" class="btn btn-primary">submit</button>
-    <p class="display-4 mt-4">{{vResult}}</p>
+    <ul class="display-4 mt-4 list-unstyled">
+      <li id="idName" style="color:#000;">{{ vResult }}</li>
+    </ul>
     <div class="alert {{vErrorTemplate}}" role="alert">
       {{vError}}
     </div>
+    
   `
 })
 
@@ -20,25 +26,31 @@ import { Component, OnInit } from '@angular/core';
 export class NameColorComponent {
 
   vResult           =   "";
+  vColor            =   "";
   vError            =   "";
   vErrorTemplate    =   "";
+  
+  
 
-  getName(){
-    
-    var vform = (<HTMLInputElement>document.getElementById("inputName")).value;    
+  public getName(){
+    this.vResult = "";
+    document.getElementById("idName").style.color = "";
+    let colorName;  
+    let vform   = (<HTMLInputElement>document.getElementById("inputName")).value;    
     //this.vResult = (<HTMLInputElement>document.getElementById("inputName")).value;
+    const objForm = Array.from(vform);
     
-/*    
-    let output  = "";
-    //console.log(objForm);
-
     for(let i = 0;i < vform.length;i++){
-      //console.log(objForm[i]);
-      output += objForm[i];
+
+      const output = Object.values(objForm[i]);
+      output["color"] = (objForm[i].charCodeAt(0) *60 +","+ vform.length*20 +","+ vform.length*10 +"");
+      
+      this.vResult += output;
+      
+      document.getElementById("idName").style.color = " rgb(" + output["color"] + ") ";
+      
     }
     
-    console.log(objForm);
-*/
 
     if(vform != ""){
       if(vform.length <= 2 && vform.length != 0){
@@ -57,4 +69,6 @@ export class NameColorComponent {
 
   }
 
+
 }
+
